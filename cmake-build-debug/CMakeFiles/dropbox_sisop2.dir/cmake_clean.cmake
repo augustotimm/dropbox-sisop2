3c240594184da06cd6863a3c8942b80682f629d2
augusto <augusto@mail.january.ai>
2022-08-03 18:57:40 -0300

    CLION cmake stuff

---
+++ cmake-build-debug/CMakeFiles/dropbox_sisop2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/dropbox_sisop2.dir/file-control/file-handler.c.o"
+  "CMakeFiles/dropbox_sisop2.dir/event-handler/event-handler.c.o"
+  "CMakeFiles/dropbox_sisop2.dir/lib/helper.c.o"
+  "dropbox_sisop2.pdb"
+  "dropbox_sisop2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/dropbox_sisop2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
